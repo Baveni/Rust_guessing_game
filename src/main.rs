a0633490175f6114b6a1da3ccd686dde1dc74e22
jernej <uranic.jernej@gmail.com>
2018-01-22 18:44:52 +0100

    added loop

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1,101);
 
     println!("The secret number is {}", secret_number);
+    loop {
+        println!("input your guess. >> ");
+        // let is to define a variable and mut is to make var mutable othervise
+        // the var is immutable
+        let mut guess = String::new();
 
-    println!("input your guess. >> ");
-    // let is to define a variable and mut is to make var mutable othervise
-    // the var is immutable
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to red line");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to red line");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win"),
+        }
     }
 }
