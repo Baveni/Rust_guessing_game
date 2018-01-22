3948d251fd5b37c199ea7da287a72e0b998e0458
jernej <uranic.jernej@gmail.com>
2018-01-22 18:28:54 +0100

    added compare

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to red line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number");
+
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Too small!");
-    Ordering::Greater => println!("Too big!");
-    Ordering::Equal => println!("You win");
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win"),
     }
 }
